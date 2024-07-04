4301156d913ba9b589e8d62dce8cab5e431399ed
trisselj <trisselj@oregonstate.edu>
2024-07-03 18:36:02 -0700

    Spacing

--- change.py
+++ change.py
@@ -2,15 +2,19 @@
 # Github Username: trisselj
 # Date: 06/26/2024
 # Description: Outputs how many of each type of coin would represent that amount with the fewest total number of coins.
+
 # Giving values to change
 Quarter = 25
 Dime = 10
 Nickel = 5
 Penny = 1
+
 # Initial input
 ChangeString = input("Enter the amount of change you have so long as it is less than a dollar")
+
 #From String to Float
 Change = float(ChangeString)
+
 # Calculating
 num_quarters = Change // Quarter
 Change %= Quarter
@@ -19,6 +23,7 @@ Change %= Dime
 num_nickels = Change // Nickel
 Change %= Nickel
 num_pennies = Change
+
 #Printing of amounts
 print("Your change in coins will be:")
 print("Quarters: " + str(num_quarters))
